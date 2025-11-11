e4333a47f254f50896c4aae7645e7c4b129ecc6d
RaoWAqar000 <raowaqar040@gmail.com>
2025-11-11 16:10:27 +0500

    Create and Move Orders and Tracking Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import CheckoutPage from "./pages/CheckoutPage";
+import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 
 function App() {
   return (
     <Routes>
       <Route index element={<Homepage />}/>
       <Route path="/checkout" element={<CheckoutPage />}/>
+      <Route path="/orders" element={<OrdersPage />}/>
+      <Route path="/tracking" element={<TrackingPage />}/>
     </Routes>
   );
 }
