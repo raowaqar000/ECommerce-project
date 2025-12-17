887c5d07619b83c014df56a5e98fb1c0ee6a9e54
RaoWAqar000 <raowaqar040@gmail.com>
2025-12-17 08:20:18 -0500

    create new TypeScript project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
@@ -14,8 +20,8 @@ export default defineConfig({
       },
     },
   },
-  build:{
+  build: {
     outDir: "../ecommerce-backend/dist",
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
 });
